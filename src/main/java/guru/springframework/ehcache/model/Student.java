2c6c9ec702fb85f37ed5d4abe3039ccc846cf4fb
Raymond Liang <rliang@incomm.com>
2021-07-08 13:58:09 -0700

    made Student with plain old java class not using lombok dependency

--- src/main/java/guru/springframework/ehcache/model/Student.java
+++ src/main/java/guru/springframework/ehcache/model/Student.java
@@ -1,13 +1,29 @@
 package guru.springframework.ehcache.model;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
-
-@RequiredArgsConstructor
-@Getter
-@ToString
 public class Student {
+    public Student(long id, String firstName, String lastName, String courseOfStudies) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.courseOfStudies = courseOfStudies;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCourseOfStudies() {
+        return courseOfStudies;
+    }
+
 
   private final long id;
   private final String firstName;
