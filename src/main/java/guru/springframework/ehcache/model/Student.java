612689b07f8d299e0dc9ff840e0ae82492519586
Raymond Liang <rliang@incomm.com>
2021-07-08 14:16:11 -0700

    removed final keywork and added getters/setters

--- src/main/java/guru/springframework/ehcache/model/Student.java
+++ src/main/java/guru/springframework/ehcache/model/Student.java
@@ -12,22 +12,37 @@ public class Student {
         return id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public String getCourseOfStudies() {
         return courseOfStudies;
     }
 
+    public void setCourseOfStudies(String courseOfStudies) {
+        this.courseOfStudies = courseOfStudies;
+    }
 
-  private final long id;
-  private final String firstName;
-  private final String lastName;
-  private final String courseOfStudies;
+    private long id;
+  private String firstName;
+  private String lastName;
+  private String courseOfStudies;
 
 }
